1e155d82619e2d72d40a22a06dc7499bd033ce5d
Bruna Alves <brunaalves080a@gmail.com>
2022-08-04 08:18:28 -0300

    .

--- main.js
+++ main.js
@@ -12,6 +12,24 @@ for (let contador = 0; contador < listaBotoesFavoritos.length; contador++){
     }
 }
 
-    function selecionarCategoria(){
-        alert('teste');
+
+    function selecionarCategoria(elementoSelecionado, categoriaSelecionada){              
+        let botoesAntesSelecionados = document.querySelectorAll('.button-teste');
+        
+        for (let contador = 0; contador < botoesAntesSelecionados.length; contador++){
+            const botao = botoesAntesSelecionados[contador];
+            botao.classList.remove('button-teste');
+        };
+        
+        elementoSelecionado.classList.add('button-teste');        
+
+        const itensDaCategoria = document.querySelectorAll('[data-categoria]');
+        itensDaCategoria.forEach( produto => {
+            produto.classList.remove('hide');
+            let categoriaDesteItem = produto.dataset.categoria;
+            if(categoriaSelecionada != categoriaDesteItem && categoriaSelecionada != 'todos') {                
+                produto.classList.add('hide');
+            };            
+        });
+
     }
