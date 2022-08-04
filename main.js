6b7b9044a32b90e1b8408aefe7bca7942e8fd16d
Bruna Alves <brunaalves080a@gmail.com>
2022-08-04 12:53:02 -0300

    site final

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const listaBotoesFavoritos = document.querySelectorAll('.favorito')
-for (let contador = 0; contador < listaBotoesFavoritos.length; contador++){
-    const botaoFavorito = listaBotoesFavoritos[contador]
+const listaFavoritado = document.querySelectorAll('.like')
+for (let contador = 0; contador < listaFavoritado.length; contador++){
+    const heart = listaFavoritado[contador]
 
-    botaoFavorito.onclick = () => {
-        if(botaoFavorito.classList[1] === 'clicado'){
-            botaoFavorito.classList.remove('clicado');
+    heart.onclick = () => {
+        if(heart.classList[1] === 'clicado'){
+            heart.classList.remove('clicado');
         }
         else{
-            botaoFavorito.classList.add('clicado');
+            heart.classList.add('clicado');
         }
     }
 }
